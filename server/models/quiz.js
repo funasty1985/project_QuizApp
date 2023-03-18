ebd39826753678f1d4f32b11620cdd1b2e414454
HOYINYIP <jones.yipkgi@gmail.com>
2023-03-18 18:57:33 -0400

    complete Backend done by Jy

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -3,7 +3,8 @@ let mongoose = require('mongoose');
 // Define the schema for the bcontacts collection
 let quizSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
-   
+    title: String,
+    description: String,
     author: String,
 }, {
   collection: 'Quiz'
