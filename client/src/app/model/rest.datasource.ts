a414248db2a6a7b10e0a9b43ac43c7ed6531e4d5
Fu <tony.f.liang@gmail.com>
2023-03-18 20:15:39 -0400

    d2

--- client/src/app/model/rest.datasource.ts
+++ client/src/app/model/rest.datasource.ts
@@ -28,7 +28,7 @@ export class RestDataSource
 
     getQuizes(): Observable<Quiz[]>
     {
-        return this.http.get<Quiz[]>(this.baseUrl + 'api/quiz');
+        return this.http.get<Quiz[]>('api/quiz');
     }
 
     private loadToken(): void
